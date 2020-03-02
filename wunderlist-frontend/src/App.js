f962e6f7559dc7b9d5a3731ecf28c6c42ced6c84
Alex Karavil <alex@karavil.io>
2020-03-02 13:27:51 -0500

    Cleaned up project file, added styled components and react-router-dom code/style

--- wunderlist-frontend/src/App.js
+++ wunderlist-frontend/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import { Switch, Route } from "react-router-dom";
+
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+const GlobalStyle = createGlobalStyle`
+   * {
+      box-sizing: border-box;
+   }
+
+   body {
+      margin: 0;
+      background: white;
+   }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <>
+         <GlobalStyle />
+
+         <Switch>
+            <Route path="/login">
+               <Login />
+            </Route>
+            <Route path="/register">
+               <Register />
+            </Route>
+            <Route path="/profile"></Route>
+         </Switch>
+      </>
+   );
 }
 
 export default App;
